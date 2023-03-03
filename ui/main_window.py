5ed5562de82098d7b56da2b2ce23a7728d23c92f
vadim <ivadimn@mail.ru>
2023-03-03 16:53:52 +0300

    new

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,18 +1,29 @@
-from PyQt6.QtWidgets import QWidget
+from PyQt6.QtWidgets import QWidget, QPushButton
 from PyQt6.QtGui import QIcon, QPixmap
 from ui.child_window import ChildWindow
+import os
 
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Pyqt6 Application")
-        self.setWindowIcon(QIcon(self.get_icon("/images/animal-dog.png")))
-        self.setFixedHeight(200)
-        self.setFixedWidth(400)
+        self.init_window()
+        self.init_ui()
 
-    def init_window(self, title: str, width: int, height: int, resizeble=(True, True), icon=None):
-        pass
+    def init_window(self):
+        self.setWindowTitle("Pyqt6 Application")
+        self.setWindowIcon(QIcon(self.get_icon("./imgs/animal-dog.png")))
+        self.setGeometry(100, 100, 500, 200)
+        self.setStyleSheet("background-color: grey")
+
+    def init_ui(self):
+        btn = QPushButton("Click me", self)
+        btn.setGeometry(20, 20, 80, 40)
+        btn.setStyleSheet("background-color: yellow")
+        btn.setIcon(QIcon("./imgs/arrow.png"))
+        print(os.path.exists("arrow.png"))
+        print(os.path.abspath(os.path.curdir))
+
 
     def init_toolbar(self):
         pass
